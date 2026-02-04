8acda12e1e0f745e55a50d727efd3520917cc677
ayushcodes3004 <aj09121973@>
2026-02-04 20:28:40 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import express from "express";
 import { chats } from "./data/data.js";
 import dotenv from "dotenv";
+import { connect } from "mongoose";
+import connectDB from "./config/db.js";
 
 const app = express();
-dotenv.config();    
+dotenv.config();
+connectDB();
 
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.get("/api/chat", (req,res)=>{
+app.get("/api/chat", (req, res) => {
     res.send(chats);
 });
 
-app.get("/api/chat/:id", (req,res)=>{
+app.get("/api/chat/:id", (req, res) => {
     const chat = chats.find((c) => c._id === req.params.id);
     res.send(chat);
 });
@@ -22,5 +25,5 @@ app.get("/api/chat/:id", (req,res)=>{
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
